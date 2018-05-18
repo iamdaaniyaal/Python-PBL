b2531e294d739cd9a295ddabacd60bb1ece510bd
Mohammed Daaniyaal <iamdaaniyaal@gmail.com>
2018-05-18 11:45:18 +0530

    Delete everything

--- anagramsapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnagramsappConfig(AppConfig):
-    name = 'anagramsapp'
--- anagramsapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class First(forms.Form):
-	answer	 = forms.CharField(max_length=500,widget=forms.TextInput(attrs={'size':50}),required=True,label='')	
